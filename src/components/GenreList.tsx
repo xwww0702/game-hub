04a670244e09cde7b50a070337817a66ff2c197b
wqw <wqw15390270020@outlook.com>
2025-05-21 21:20:53 +0100

    clean up Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,6 +9,7 @@ import {
   List,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import { debounce } from "lodash";
 interface Props {
@@ -25,22 +26,57 @@ const GenreList = ({ onSelectGenre, selecedGenre }: Props) => {
   if (error) return null;
   return (
     <>
+      <Heading fontSize="lg">Genres</Heading>
       <List.Root listStyle={"none"}>
         {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
+          // <ListItem key={genre.id} paddingY="5px" max-width="100%">
+          //   <HStack gap={2}>
+          //     <Image
+          //       boxSize="32px"
+          //       borderRadius={8}
+          //       objectFit="cover"
+          //       src={getCroppedImageUrl(genre.image_background)}
+          //     ></Image>
+          //     <Button
+          //       onClick={() => {
+          //         handleSelectGenre(genre);
+          //       }}
+          //       fontWeight={genre.id === selecedGenre?.id ? "bold" : "normal"}
+          //       fontSize="lg"
+          //       width="120px"
+          //       variant="solid"
+          //       whiteSpace="normal"
+          //       textAlign="left"
+          //       p="0"
+          //       m="0"
+          //       px="0"
+          //       py="0"
+          //     >
+          //       {genre.name}
+          //     </Button>
+          //   </HStack>
+          // </ListItem>
+          <ListItem key={genre.id} py="5px" maxW="100%">
+            <HStack gap={2} align="center">
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
-              ></Image>
+              />
+              {/* 按钮里面的文字一直没有对齐，justifyContent="flex-start"，加了这个就对齐了 */}
               <Button
-                onClick={() => {
-                  handleSelectGenre(genre);
-                }}
+                onClick={() => handleSelectGenre(genre)}
                 fontWeight={genre.id === selecedGenre?.id ? "bold" : "normal"}
-                fontSize="lg"
-                variant="plain"
+                fontSize="md"
+                variant="ghost"
+                justifyContent="flex-start"
+                textAlign="left"
+                whiteSpace="normal"
+                w="120px"
+                h="auto"
+                minW="unset"
+                p="0"
               >
                 {genre.name}
               </Button>
