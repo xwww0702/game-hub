7afd745015bceec93efd3f29cfec7b76cd37c397
wqw <wqw15390270020@outlook.com>
2025-05-21 17:50:48 +0100

    handle games with no image

--- src/services/image_url.ts
+++ src/services/image_url.ts
@@ -1,5 +1,6 @@
+import placeholder from '../assets/placeholder.jpg'
 const getCroppedImageUrl =(url:string)=>{
-    if(!url) return ''
+    if(!url) return placeholder
     const target= 'media/'
     const index = url.indexOf(target)+target.length
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
