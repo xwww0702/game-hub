28f8cc10d3b6c2a42ade5ce081ddba5d6a751481
wqw <wqw15390270020@outlook.com>
2025-05-27 10:41:56 +0100

    expand game card click area

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,26 +11,28 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root
-      _hover={{
-        transform: "scale(1.03)",
-        transition: "transform .15s ease-in",
-      }}
-    >
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <Card.Body>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <Platform_icon
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          ></Platform_icon>
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Card.Title fontSize="2xl">
-          <Link to={`/games/${game.slug}`}>{game.name}</Link>
-          <Emoji rating={game.rating_top} />
-        </Card.Title>
-      </Card.Body>
-    </Card.Root>
+    <Link to={`/games/${game.slug}`}>
+      <Card.Root
+        _hover={{
+          transform: "scale(1.03)",
+          transition: "transform .15s ease-in",
+        }}
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <Card.Body>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <Platform_icon
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            ></Platform_icon>
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Card.Title fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Card.Title>
+        </Card.Body>
+      </Card.Root>
+    </Link>
   );
 };
 
